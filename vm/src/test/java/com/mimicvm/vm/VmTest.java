d1438c1e4d9d452d3b7a4f7252b900ea44b3eb5a
bird <264618980+birdopening@users.noreply.github.com>
2026-06-30 22:35:42 +0200

    Create VmTest

---
+++ vm/src/test/java/com/mimicvm/vm/VmTest.java
@@ -0,0 +1,26 @@
+package com.mimicvm.vm;
+
+import com.mimicvm.shared.method.VMethod;
+import com.mimicvm.shared.op.Opcodes;
+import com.mimicvm.shared.type.Value;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class VmTest implements Opcodes {
+
+    @Test
+    void test1() {
+
+        final byte[] insns = {
+                (byte) I32_CONST, 0x0, 0x0, 0x0, 0x2,
+                (byte) I32_CONST, 0x0, 0x0, 0x0, 0x0B, // 11
+                (byte) I32_ADD,
+                (byte) RETURN
+        };
+
+
+        final Value result = new Interpreter(new VMethod(2, 0, insns)).run();
+        assertEquals(Value.i32(13), result);
+    }
+}

ad448714ca000a0bd3a1ca5a7ad17e0dd98dd33b
bird <264618980+lifusdev@users.noreply.github.com>
2026-08-03 17:55:54 +0200

    feat(compiler): remaining VModule tables

--- compiler/src/main/java/com/mimicvm/compiler/ModuleWriter.java
+++ compiler/src/main/java/com/mimicvm/compiler/ModuleWriter.java
@@ -1,7 +1,12 @@
 package com.mimicvm.compiler;
 
+import com.mimicvm.shared.call.CtorCall;
+import com.mimicvm.shared.call.ICall;
+import com.mimicvm.shared.call.InstCall;
+import com.mimicvm.shared.call.StaticCall;
 import com.mimicvm.shared.code.VMethod;
 import com.mimicvm.shared.code.VModule;
+import com.mimicvm.shared.type.Type;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
@@ -9,8 +14,15 @@ import java.io.IOException;
 
 public final class ModuleWriter {
 
-    static final int MAGIC = 0x4D4D564D; // MMVM
-    static final int VERSION = 1;
+    public static final int MAGIC = 0x4D4D564D; // MMVM
+    public static final int VERSION = 1;
+
+    /**
+     * ALL CALL TYPES
+     */
+    public static final int K_STATIC = 0x0;
+    public static final int K_INSTANCE = 0x1;
+    public static final int K_CTOR = 0x2;
 
     public byte[] write(VModule module) {
         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -22,6 +34,19 @@ public final class ModuleWriter {
 
             // Method table
             writeMethods(dos, module);
+
+            // types
+            writeStrings(dos, module.typeNames());
+
+            // String pool
+            writeStrings(dos, module.constants());
+
+            // field types
+            writeTypes(dos, module.fieldTypes());
+            writeTypes(dos, module.staticTypes());
+
+            // Call table
+            writeCalls(dos, module.calls());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,8 +69,56 @@ public final class ModuleWriter {
         dos.writeInt(method.maxLocals());
 
         final byte[] insns = method.insns();
+
         // length first for reader
         dos.writeInt(insns.length);
         dos.write(insns);
+    }
+
+    private void writeStrings(DataOutputStream dos, String[] strings) throws IOException {
+        dos.writeInt(strings.length);
+
+        for (String s : strings) {
+            dos.writeUTF(s);
+        }
+    }
+
+    private void writeTypes(DataOutputStream dos, Type[] types) throws IOException {
+        dos.writeInt(types.length);
+
+        for (Type type : types) {
+            dos.writeByte(type.ordinal());
+        }
+    }
+
+
+    private void writeCalls(DataOutputStream dos, ICall[] calls) throws IOException {
+        dos.writeInt(calls.length);
+
+        for (ICall call : calls) {
+            writeCall(dos, call);
+        }
+    }
+
+    private void writeCall(DataOutputStream dos, ICall call) throws IOException {
+        if (call instanceof StaticCall) {
+            dos.writeByte(K_STATIC);
+            dos.writeUTF(call.owner());
+            dos.writeUTF(call.name());
+            dos.writeUTF(call.desc());
+        } else if (call instanceof InstCall) {
+            dos.writeByte(K_INSTANCE);
+            dos.writeUTF(call.owner());
+            dos.writeUTF(call.name());
+            dos.writeUTF(call.desc());
+        } else if (call instanceof CtorCall) {
+            dos.writeByte(K_CTOR);
+            // the name is ALWAYS <init>,
+            // so there is no need to save it
+            dos.writeUTF(call.owner());
+            dos.writeUTF(call.desc());
+        } else {
+            throw new IllegalArgumentException("Unknown call type: " + call.getClass());
+        }
     }
 }
